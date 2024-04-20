b7a075dc8ce2a8a264842ceb63b91f3c12bfd604
Fernanda Avendaño <fernanda.avendano@sansano.usm.cl>
2024-04-20 14:16:55 -0400

    Generacion municion

--- server/tierra.go
+++ server/tierra.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"time"
 
 	pb "github.com/dmoyan0/TareasSD/tree/main/proto"
 	"google.golang.org/grpc"
@@ -14,28 +15,51 @@ type server struct {
 	pb.UnimplementedWishListServiceServer     //<-Interfaz servicio gRPC
 	municionAT                            int //Contador Munición Anti-Terrestre
 	municionMP                            int //Contador Munición Pesada
+	maxAT                                 int
+	maxMP                                 int
 }
 
-func (s *server) solicitarM(ctx context.Context, request *pb.Request) (*pb.Response, error) {
+func (s *server) SolicitarM(ctx context.Context, request *pb.Request) (*pb.Response, error) {
 	fmt.Printf("Recepción de solicitud desde equipo %d, %d AT y %d MP\n", request.ID, request.AT, request.MP)
 
-	//Verifiamos munición
-	if s.municionAT >= int(request.AT) {
-		if s.municionMP >= int(request.MP) {
-			//Actializamos contadores
-			s.municionAT -= int(request.AT)
-			s.municionMP -= int(request.MP)
-			//Exito
-			fmt.Printf("AT EN SISTEMA: %d; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
-			return &pb.response{Success: true}, nil
-		}
-		//No hay munición suficiente
-		fmt.Printf("AT EN SISTEMA: %d ; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
-		return &pb.response{Success: false}, nil
+	// Verificamos munición
+	if s.municionAT >= int(request.AT) && s.municionMP >= int(request.MP) {
+		// Actualizamos contadores
+		s.municionAT -= int(request.AT)
+		s.municionMP -= int(request.MP)
+		// Éxito
+		fmt.Printf("AT EN SISTEMA: %d; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
+		return &pb.Response{Success: true}, nil
 	}
-	//No hay munición suficiente
-	fmt.Printf("AT EN SISTEMA: %d ; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
-	return &pb.response{Success: false}, nil
+
+	// No hay suficiente munición
+	fmt.Printf("AT EN SISTEMA: %d; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
+	return &pb.Response{Success: false}, nil
+}
+
+func (s *server) generarMunicion() {
+	for {
+		// Generamos munición cada 5 seg
+		time.Sleep(5 * time.Second)
+
+		// Vemos si hay espacio para la municion AT
+		if s.municionAT < s.maxAT {
+			if s.municionAT+10 <= s.maxAT {
+				s.municionAT += 10
+			} else {
+				s.municionAT = s.maxAT
+			}
+		}
+
+		// Vemos si hay espacio para la municion MP
+		if s.municionMP < s.maxMP {
+			if s.municionMP+5 <= s.maxMP {
+				s.municionMP += 5
+			} else {
+				s.municionMP = s.maxMP
+			}
+		}
+	}
 }
 
 func main() {
@@ -47,8 +71,13 @@ func main() {
 	}
 
 	serv := grpc.NewServer()
-	server := &server{municionAT: 0, municionMP: 0} //Municiones parten en 0
+	server := &server{municionAT: 0, municionMP: 0, maxAT: 50, maxMP: 20} //Municiones parten en 0
 	pb.RegisterWishListServiceServer(serv, &server)
+
+	//Generamos munición
+	go server.generarMunicion()
+
+	//Error
 	if err = serv.Serve(listner); err != nil {
 		panic("cannot initialize the server" + err.Error())
 	}
